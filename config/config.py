e035c99ae1d667537d40fe0b321af10068578aeb
NesoDev <ever.avendano@unmsm.edu.pe>
2024-10-03 23:34:16 -0500

    add data for roobet casino in config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,18 @@
-
+DATA_CASINOS = {
+    "roobet": {
+        "main_url": "https://roobet.com",
+        "games": {
+            "lightning_roulette": {
+                "name": "Lightning Roulette",
+                "game_url": "https://roobet.com/game/evolution:lightning_roulette",
+                "login_url": "https://roobet.com/game/evolution:lightning_roulette?modal=auth&tab=login"
+            },
+        },
+        "accounts": [
+            {
+                "email": "diegoafarrua",
+                "password": "N36root654$$"
+            }
+        ]
+    }
+}
